790d841bb412816acb43f66f1ddd38cb90875a3f
Unheppcat <unheppcat@yahoo.com>
2025-12-26 20:20:52 -0600

    Adds a ddev installation to provide containers to run the application locally

--- application/public/index.php
+++ application/public/index.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection UnknownInspectionInspection */
 
 use App\Kernel;
 use Symfony\Component\Dotenv\Dotenv;
@@ -7,27 +7,30 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if ($_SERVER['APP_ENV'] !== 'prod') {
+if (!isset($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] !== 'prod') {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
 
     Debug::enable();
 }
 
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-date_default_timezone_set($_SERVER['APP_TIMEZONE']);
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) ($_SERVER['APP_DEBUG'] ?? false));
+date_default_timezone_set($_SERVER['APP_TIMEZONE'] ?? 'UTC');
 
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
 $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if($_SERVER['APP_ENV'] === 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+if(($_SERVER['APP_ENV'] ?? 'dev') === 'prod') {
+    $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+}
 if($trustedProxies) {
     Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
 
 $request = Request::createFromGlobals();
+/** @noinspection PhpUnhandledExceptionInspection */
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
